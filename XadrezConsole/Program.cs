88fc707893f36b8d771cafd06d23aeb22eff49f9
Murilo Rerison de Sousa Paz <murilokapaz@gmail.com>
2019-08-17 19:10:17 -0300

    mudando cores das pecas

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -7,11 +7,21 @@ using XadrezConsole.Tabuleiro.Exceptions;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine();
-            Console.WriteLine(pos.ToPosicao());
+            try {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(3,5));
+
+                Tela.imprimirTabuleiro(tab);
+
+                Console.ReadLine();
+            }
+            catch(TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- XadrezConsole/Tela.cs
+++ XadrezConsole/Tela.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ns.tabuleiro;
+using XadrezConsole.Tabuleiro.Enums;
 
 namespace xadrez_console {
     class Tela {
@@ -7,16 +8,39 @@ namespace xadrez_console {
         public static void imprimirTabuleiro(Tabuleiro tab) {
 
             for (int i = 0; i < tab.Linhas; i++) {
+                Console.Write($"{8-i} ");
                 for (int j = 0; j < tab.Colunas; j++) {
                     if (tab.Peca(i, j) == null) {
                         Console.Write("- ");
                     }
                     else {
-                        Console.Write(tab.Peca(i, j) + " ");
+                        ImprimirPeca(tab.Peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
+
+
+
+
         }
+        public static void ImprimirPeca(Peca peca) {
+            if (peca.cor == Cor.Branca) {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+            else {
+                ConsoleColor aux = Console.ForegroundColor;//cor padrão
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+
+            }
+        }
+
     }
 }
